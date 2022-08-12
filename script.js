bb76028278e3e9b3fcf8a350795296258fd21a3a
Muxammad <markeloffnavi21@gmail.com>
2022-08-12 17:28:07 +0500

    index.html

---
+++ script.js
@@ -0,0 +1,104 @@
+/* Yow */
+
+var yow = +prompt('yowingizni kiriting')
+
+if(yow <= 18) {
+    alert('sizning yowingiz ' + yow + ' oqiwingiz kere')
+}else if(yow <= 50) {
+    alert('sizni yowingz ' + yow + ' iwlawingiz kere')
+}else if(yow <= 59) {
+    alert('sizning yowingz ' + yow + ' sz yaqnda nafaqaga ciqasz')
+}else if(yow <= 150) {
+    alert('sizning yowingz ' + yow + ' sz nafaqaxorsz ')
+}else{
+    alert(' sz xato yozdz!')
+}
+
+/* Sogot */
+
+// var voxt = +prompt('soati kiritin')
+
+
+// if(voxt == 24) {
+//     alert('soat tungi ' + (voxt - 12))
+// }else if(voxt == 23) {
+//     alert('sogot kecki ' + (voxt - 12))
+// }else if(voxt == 22) {
+//     alert('sogot kecki ' + (voxt - 12))
+// }else if(voxt == 21) {
+//     alert('sogot kecki ' + (voxt - 12))
+// }else if(voxt == 20) {
+//     alert('sogot kecki ' + (voxt - 12))
+// }else if(voxt == 19) {
+//     alert('sogot kecki ' + (voxt - 12))
+// }else if(voxt == 18) {
+//     alert('sogot asrgi ' + (voxt - 12))
+// }else if(voxt == 17) {
+//     alert('sogot asrgi ' + (voxt - 12))
+// }else if(voxt == 16) {
+//     alert('sogot asrgi ' + (voxt - 12))
+// }else if(voxt == 15) {
+//     alert('sogot abedgi ' + (voxt - 12))
+// }else if(voxt == 14) {
+//     alert('sogot abedgi ' + (voxt - 12))
+// }else if(voxt == 13) {
+//     alert('sogot abedgi ' + (voxt - 12))
+// }else if(voxt == 12) {
+//     alert('sogot abedgi ' + voxt)
+// }else if(voxt == 11) {
+//     alert('sogot abedgi ' + voxt)
+// }else if(voxt == 10) {
+//     alert('sogot ertalab ' + voxt)
+// }else if(voxt == 9) {
+//     alert('sogot ertalab ' + voxt)
+// }else if(voxt == 8) {
+//     alert('sogot ertalab ' + voxt)
+// }else if(voxt == 7) {
+//     alert('sogot ertalab ' + voxt)
+// }else if(voxt == 6) {
+//     alert('sogot tongi ' + voxt)
+// }else if(voxt == 5) {
+//     alert('sogot tongi ' + voxt)
+// }else if(voxt == 4) {
+//     alert('sogot tongi ' + voxt)
+// }else if(voxt == 3) {
+//     alert('soat tungi ' + voxt)
+// }else if(voxt == 2) {
+//     alert('soat tungi ' + voxt)
+// }else if(voxt == 1) {
+//     alert('soat tungi ' + voxt)
+// }else{
+//     alert('xato !!!')
+// }
+
+/* 3 Tali son */
+
+// var num1 = +prompt('birinc soni kiriting')
+// var num2 = +prompt('ikinci soni kiriting')
+// var num3 = +prompt('ucinci soni kiriting')
+
+// if(num1 > num2 && num1 < num3 || num1 < num2 && num1 > num3) {
+//     alert('ortaca son ' + num1)
+// }else if(num2 > num1 && num2 < num3 || num2 < num1 && num2 > num3) {
+//     alert('ortacha son ' + num2)
+// }else if(num3 > num2 && num3 < num1 || num3 < num2 && num3 > num2) {
+//     alert('ortacha son ' + num3)
+// }else if(isNaN(num1) && isNaN(num2)) {
+//     alert('bir bn iki xato!!!')
+// }else if(isNaN(num1) && isNaN(num3)) {
+//     alert('bir bn uch xato!!!')
+// }else if(isNaN(num2) && isNaN(num1)) {
+//     alert('ikki bn br xato!!!')
+// }else if(isNaN(num2) && isNaN(num3)) {
+//     alert('iki bn uch xato!!!')
+// }else if(isNaN(num3) && isNaN(num1)) {
+//     alert('uch bn br xato!!!')
+// }else if(isNaN(num3) && isNaN(num2)) {
+//     alert('uch bn iki xato!!!')
+// }else if(isNaN(num1)) {
+//     alert('birincisi xato!')
+// }else if(isNaN(num2)) {
+//     alert('ikincisi xato!')
+// }else if(isNaN(num3)) {
+//     alert('ucincisi xato!')
+// }
